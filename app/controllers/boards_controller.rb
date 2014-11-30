70ad18d4400074eff568bbc17abf920ecb0da56a
j.taneichi <j.tane3ma@gmail.com>
2014-11-30 09:54:28 +0900

    create new

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -3,8 +3,18 @@ class BoardsController < ApplicationController
   end
 
   def new
+    @board = Board.new
   end
 
   def create
+    @board = Board.new(params_board)
+    @board.save
+    redirect_to "/boards"
+  end
+
+  private
+
+  def params_board
+    params.permit(:title, :editor)
   end
 end
